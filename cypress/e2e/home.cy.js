63dfe871da37228807afa071761162d2362ffe3c
RebecaCamilo <rebecacamilo3@gmail.com>
2024-08-26 23:39:14 -0300

    refac: add text plan info in fixtures file

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,9 @@
+import home from "../fixtures/home.json"
+
 describe('home', () => {
   it('webapp should be online', () => {
     cy.visit('http://localhost:3000')
 
-    cy.title('eq', 'Gerencie suas tarefas com Mark L')
+    cy.title('eq', home.title)
   })
 })
