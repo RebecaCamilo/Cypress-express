2fab5dd1ccc38f7cc3ef5c3606b9af9cb33810e1
RebecaCamilo <rebecacamilo3@gmail.com>
2024-08-28 23:16:30 -0300

    feat: add songs ramdonmly

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
+import {faker} from '@faker-js/faker';
+
 describe("tasks", () => {
     it("deve cadastrar uma nova tarefa", () => {
         cy.visit("http://localhost:3000");
 
         cy.get('input[placeholder="Add a new Task"]')
-          .type("Walk the dogs");
+          .type(faker.music.songName());
 
         //xpath: '//button[contains(text(), "Create")]'
         cy.contains('button', 'Create').click();
