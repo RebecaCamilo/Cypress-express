cade407eff6c09ba86319d8140fc51853d48c224
RebecaCamilo <rebecacamilo3@gmail.com>
2024-08-29 22:46:35 -0300

    refac: add api request to clean tasks before add them

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,16 +1,21 @@
 /// <reference types="cypress" />
 
-import {faker} from '@faker-js/faker';
-
 describe("tasks", () => {
-    it("deve cadastrar uma nova tarefa", () => {
-        cy.visit("http://localhost:3000");
+  it("deve cadastrar uma nova tarefa", () => {
+    var taskDescription = "Walk the dog";
 
-        cy.get('input[placeholder="Add a new Task"]')
-          .type(faker.music.songName());
-
-        //xpath: '//button[contains(text(), "Create")]'
-        cy.contains('button', 'Create').click();
-        
+    cy.request({
+      url: "http://localhost:3333/helper/tasks/",
+      method: "DELETE",
+      body: { name: taskDescription },
+    }).then((response) => {
+      expect(response.status).to.eq(204);
     });
+    cy.visit("http://localhost:3000");
+
+    cy.get('input[placeholder="Add a new Task"]').type(taskDescription);
+
+    //xpath: '//button[contains(text(), "Create")]'
+    cy.contains("button", "Create").click();
+  });
 });
