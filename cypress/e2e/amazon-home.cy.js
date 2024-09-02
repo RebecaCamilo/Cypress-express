f41c504197c48e43f3ee3807ecbe9e70294ab187
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-01 21:05:30 -0300

    feat: validate the first element in list

--- cypress/e2e/amazon-home.cy.js
+++ cypress/e2e/amazon-home.cy.js
@@ -4,11 +4,15 @@ import {faker} from '@faker-js/faker';
 
 describe("home", () => {
     it("should search for an item", () => {
+      var productName = faker.commerce.product();
+
         cy.visit("https://www.amazon.com/");
 
         cy.get('input[aria-label="Search Amazon"]')
-          .type(faker.commerce.product());
+          .type(productName);
 
-        cy.get('#nav-search-submit-button').click();        
+        cy.get('#nav-search-submit-button').click();
+
+        cy.contains('div[data-index]', productName).should('be.visible');
     });
 });
