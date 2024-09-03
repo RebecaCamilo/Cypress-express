32d3b27e64494c844ec94e493c51dc0ce1ae2c88
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-02 23:03:07 -0300

    feat: add test case for duplicated task

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,7 +2,7 @@
 
 describe("tasks", () => {
   it("deve cadastrar uma nova tarefa", () => {
-    var taskDescription = "Walk the dog";
+    const taskDescription = "Walk the dog";
 
     cy.request({
       url: "http://localhost:3333/helper/tasks/",
@@ -11,6 +11,7 @@ describe("tasks", () => {
     }).then((response) => {
       expect(response.status).to.eq(204);
     });
+
     cy.visit("http://localhost:3000");
 
     cy.get('input[placeholder="Add a new Task"]').type(taskDescription);
@@ -19,5 +20,40 @@ describe("tasks", () => {
     cy.contains("button", "Create").click();
 
     cy.contains('main p', taskDescription).should('be.visible');
+  });
+
+  it("não deve permitir tarefa duplicada", () => {
+    const task = {
+      name: 'Estudar inglês', 
+      is_done: false
+    };
+
+    cy.request({
+      url: "http://localhost:3333/helper/tasks/",
+      method: "DELETE",
+      body: { name: task.name },
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });''
+
+    cy.request({
+      url: "http://localhost:3333/tasks/",
+      method: "POST",
+      body: task,
+    }).then(response => {
+      expect(response.status).to.eq(201);
+    });
+
+    cy.visit("http://localhost:3000");
+
+    // cy.get('input[placeholder="Add a new Task"]').type(taskDescription);    
+    // cy.contains("button", "Create").click();
+
+    cy.get('input[placeholder="Add a new Task"]').type(task.name);    
+    cy.contains("button", "Create").click();
+
+    cy.get('.swal2-html-container')
+      .should('be.visible')
+      .should('have.text', 'Task already exists!');
   });
 });
