3069926296c5dc8a4c1f44844380c26af1e52e2f
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-03 22:49:38 -0300

    refac: add commands functions in command layer

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -28,32 +28,3 @@ describe("tasks", () => {
       .should('have.text', 'Task already exists!');
   });
 });
-
-Cypress.Commands.add('createTask', (taskDescription) => {
-  cy.visit("http://localhost:3000");
-
-    cy.get('input[placeholder="Add a new Task"]').type(taskDescription);
-
-    //xpath: '//button[contains(text(), "Create")]'
-    cy.contains("button", "Create").click();
-});
-
-Cypress.Commands.add('removeTaskByDescription', (taskDescription, status) => {
-  cy.request({
-    url: "http://localhost:3333/helper/tasks/",
-    method: "DELETE",
-    body: { name: taskDescription },
-  }).then((response) => {
-    expect(response.status).to.eq(204);
-  });
-});
-
-Cypress.Commands.add('postTask', (task) => {
-  cy.request({
-      url: "http://localhost:3333/tasks/",
-      method: "POST",
-      body: task,
-    }).then(response => {
-      expect(response.status).to.eq(201);
-    });
-});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('createTask', (taskDescription) => {
+    cy.visit("http://localhost:3000");
+  
+      cy.get('input[placeholder="Add a new Task"]').type(taskDescription);
+  
+      //xpath: '//button[contains(text(), "Create")]'
+      cy.contains("button", "Create").click();
+  });
+  
+  Cypress.Commands.add('removeTaskByDescription', (taskDescription, status) => {
+    cy.request({
+      url: "http://localhost:3333/helper/tasks/",
+      method: "DELETE",
+      body: { name: taskDescription },
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+  
+  Cypress.Commands.add('postTask', (task) => {
+    cy.request({
+        url: "http://localhost:3333/tasks/",
+        method: "POST",
+        body: task,
+      }).then(response => {
+        expect(response.status).to.eq(201);
+      });
+  });
