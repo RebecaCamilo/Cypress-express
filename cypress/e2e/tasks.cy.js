8579aab8254aff669d36bfda776842fb349eb027
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-05 22:32:16 -0300

    refac: add test cases in a context

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,39 +1,36 @@
 /// <reference types="cypress" />
 
 describe("tasks", () => {
-  it("deve cadastrar uma nova tarefa", () => {
-    const taskDescription = "Walk the dog";
-
-    cy.removeTaskByDescription(taskDescription);
-
-    cy.createTask(taskDescription);
-
-    cy.contains('main p', taskDescription).should('be.visible');
-  });
-
-  it("não deve permitir tarefa duplicada", () => {
-    const task = {
-      name: 'Estudar inglês', 
-      is_done: false
-    };
-
-    cy.removeTaskByDescription(task.name);
-
-    cy.postTask(task);
-
-    cy.createTask(task.name);
-
-    cy.get('.swal2-html-container')
-      .should('be.visible')
-      .should('have.text', 'Task already exists!');
-  });
-
-  it("não deve permitir tarefa vazia", () => {
-    
-    cy.createTask();
-
-    cy.isRequired('This is a required field');    
-
-  });
-
+  context('cadastro', () => {
+    it("deve cadastrar uma nova tarefa", () => {
+      const taskDescription = "Walk the dog";
+  
+      cy.removeTaskByDescription(taskDescription);
+      cy.createTask(taskDescription);
+  
+      cy.contains('main p', taskDescription)
+        .should('be.visible');
+    });
+  
+    it("não deve permitir tarefa duplicada", () => {
+      const task = {
+        name: 'Estudar inglês', 
+        is_done: false
+      };
+  
+      cy.removeTaskByDescription(task.name);
+      cy.postTask(task);
+      cy.createTask(task.name);
+  
+      cy.get('.swal2-html-container')
+        .should('be.visible')
+        .should('have.text', 'Task already exists!');
+    });
+  
+    it("não deve permitir tarefa vazia", () => {
+      
+      cy.createTask();
+      cy.isRequired('This is a required field');
+    });
+  })
 });
