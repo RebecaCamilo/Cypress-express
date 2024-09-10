774140d3d637bcbe2d134c5de71dfcf94b18ee0d
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-09 23:26:24 -0300

    refac: format code

--- cypress/e2e/amazon-home.cy.js
+++ cypress/e2e/amazon-home.cy.js
@@ -12,27 +12,25 @@ describe("home", () => {
 
     cy.get("#nav-search-submit-button").click();
 
-    cy.contains('div[data-index]', productName)
-      .should('be.visible');
+    cy.contains("div[data-index]", productName).should("be.visible");
 
     var cont = 0;
 
+    cy.get("div .s-result-list > div")
+      .each(($row) => {
+        // Dentro de cada linha, encontra o h2 e então o span
+        const productName = $row.find("h2 span").text().toLowerCase();
 
+        // Verifica se o span é visível
+        cy.wrap($row.find("h2 span")).should("be.visible"); //.title-recipe
 
-    cy.get('div .s-result-list > div').each(($row) => { 
-      // Dentro de cada linha, encontra o h2 e então o span
-      const productName = $row.find('h2 span').text().toLowerCase();
-
-      // Verifica se o span é visível
-      cy.wrap($row.find('h2 span')).should("be.visible"); //.title-recipe
-      
-      // Verifica se o texto do span contém "chair"
-      if (productName.includes(productName)) {
+        // Verifica se o texto do span contém "chair"
+        if (productName.includes(productName)) {
           cont++;
-      }
-  }).then(() => {
-      cy.log(`Total de itens com ${productName}: ` + cont);
-  });
-
+        }
+      })
+      .then(() => {
+        cy.log(`Total de itens com ${productName}: ` + cont);
+      });
   });
 });
