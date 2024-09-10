cfceaa446a4e06c8da34bbebff9a48960a117c67
RebecaCamilo <rebecacamilo3@gmail.com>
2024-09-09 23:25:51 -0300

    refac: try to use cypress commands

--- cypress/e2e/amazon-home.cy.js
+++ cypress/e2e/amazon-home.cy.js
@@ -18,16 +18,13 @@ describe("home", () => {
     var cont = 0;
 
 
-//     cy.get('div .s-result-list').each(($row) => { //div[data-index][data-uuid]
-// //se contiver h2 ????
-//       if ($row.find('h2').eq(0).text() == 'Skate2.0') {
-//         $row.find('td').eq(5).children('div').children('.btn-danger').console.log();
-//       }
-//     })
 
     cy.get('div .s-result-list > div').each(($row) => { 
       // Dentro de cada linha, encontra o h2 e então o span
       const productName = $row.find('h2 span').text().toLowerCase();
+
+      // Verifica se o span é visível
+      cy.wrap($row.find('h2 span')).should("be.visible"); //.title-recipe
       
       // Verifica se o texto do span contém "chair"
       if (productName.includes(productName)) {
@@ -37,13 +34,5 @@ describe("home", () => {
       cy.log(`Total de itens com ${productName}: ` + cont);
   });
 
-    // Seleciona todos os produtos da lista e valida se o nome do produto está presente em cada um
-    // cy.get("div[data-index]").should("have.length.greaterThan", 0); // Espera até que haja pelo menos um item na lista
-    // cy.get("div[data-index]").each(($el) => {
-    //   cy.wrap($el)
-    //     .contains(productName, { timeout: 10000 })
-    //     .should("be.visible");
-        
-    // });
   });
 });
